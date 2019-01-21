8fc4ba363e52312e22e05bc9bccd0b5347d05c12
hassehulabeck <hansbudandersson@gmail.com>
2019-01-21 11:31:40 +0100

    First

---
+++ luckor.js
@@ -0,0 +1,128 @@
+var luckor = [
+  {
+    id: 1,
+    color: "red",
+    vinst: 100
+  },
+  {
+    id: 2,
+    color: "red",
+    vinst: 1000
+  },
+  {
+    id: 3,
+    color: "red",
+    vinst: 10000
+  },
+  {
+    id: 4,
+    color: "red",
+    vinst: 100000
+  },
+  {
+    id: 5,
+    color: "red",
+    vinst: 1000000
+  },
+  {
+    id: 6,
+    color: "blue",
+    vinst: "Resa till Olskroken"
+  },
+  {
+    id: 7,
+    color: "blue",
+    vinst: "Resa till Ulricehamn"
+  },
+  {
+    id: 8,
+    color: "blue",
+    vinst: "Resa till Malmö"
+  },
+  {
+    id: 9,
+    color: "blue",
+    vinst: "Resa till Köpenhamn"
+  },
+  {
+    id: 10,
+    color: "blue",
+    vinst: "Resa till New York"
+  },
+  {
+    id: 11,
+    color: "yellow",
+    vinst: "Elvisp"
+  },
+  {
+    id: 12,
+    color: "yellow",
+    vinst: "Köksassistent"
+  },
+  {
+    id: 13,
+    color: "yellow",
+    vinst: "Kaffekvarn"
+  },
+  {
+    id: 14,
+    color: "yellow",
+    vinst: "Dammsugare"
+  },
+  {
+    id: 15,
+    color: "yellow",
+    vinst: "Lyxdammsugare"
+  },
+  {
+    id: 16,
+    color: "green",
+    vinst: "1 paket bacon"
+  },
+  {
+    id: 17,
+    color: "green",
+    vinst: "10 paket bacon"
+  },
+  {
+    id: 18,
+    color: "green",
+    vinst: "En månads baconkonsumtion"
+  },
+  {
+    id: 19,
+    color: "green",
+    vinst: "Ett års bacon"
+  },
+  {
+    id: 20,
+    color: "green",
+    vinst: "Din vikt i bacon varje vecka!!!"
+  },
+  {
+    id: 21,
+    color: "white",
+    vinst: "En pinnstol"
+  },
+  {
+    id: 22,
+    color: "white",
+    vinst: "En pinnsoffa"
+  },
+  {
+    id: 23,
+    color: "white",
+    vinst: "En soffa"
+  },
+  {
+    id: 24,
+    color: "white",
+    vinst: "En deluxe-säng"
+  },
+  {
+    id: 25,
+    color: "white",
+    vinst: "Ett möblerat rum"
+  }
+];
+export default luckor;
